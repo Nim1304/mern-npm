2b8f78837c782affb01eaddcbde73c55268256e2
Nimish Shah <nimishshah2000@gmail.com>
2020-04-17 19:06:50 +0530

    gitignore

--- index.js
+++ index.js
@@ -6,29 +6,39 @@ const _cwd = process.cwd();
 const fs = require('fs-extra');
 
 const createDir = (name)=>{
-    spawn('mkdir',[`${name}`],{
-        cwd:process.cwd()
-    })
+    return new Promise((resolve,reject)=>{
+        fs.mkdirs(path.join(_cwd,`${name}`)).then(()=>{
+            resolve('Directory Created')
+        }).catch(err=> reject(err));
+    });
 }
 
-
+const createReactApp = (name)=>{
+    return new Promise((resolve,reject)=>{
+        spawn('npx',['create-react-app','client'],{
+            cwd: path.join(_cwd,name),
+            stdio:"inherit"
+        })
+        resolve('success');
+    })
+}
 const run = async () => {
     const questions = await inquirer.prompt([
         {
             type: 'input',
             name: 'projectName',
             message: 'What is the name of the project? (Use small letters and without space)'
-        },
-        {
-            type: 'list',
-            name: 'frontend1',
-            message: 'What frontend do you want to use?',
-            choices: [{ name: "Yes", value: "yes" }, { name: "No", value: "no" }]
         }
     ]);
-    console.log(questions);
-    createDir('hi');
-    fs.copySync(path.join(__dirname,'template/'),path.join(_cwd,'hi'));
+    // console.log(questions);
+    // var a=spawn('ls',['-al'],{
+    //     cwd:path.join(_cwd,'template'),
+    //     stdio:"inherit"
+    // });
+    // console.log(a);
+    await createDir(questions.projectName);
+    // await createReactApp(questions.projectName);
+    fs.copySync(path.join(__dirname,'template/'),path.join(_cwd,questions.projectName));
 
 }
 
