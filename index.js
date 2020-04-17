75d0dbc40db20d95d333212b503912f2f1edbaf8
Nimish Shah <nimishshah2000@gmail.com>
2020-04-17 19:46:24 +0530

    async-await bug

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const path = require('path');
 const _cwd = process.cwd();
 const fs = require('fs-extra');
 
-const createDir = (name)=>{
+const createDir =(name)=>{
     return new Promise((resolve,reject)=>{
         fs.mkdirs(path.join(_cwd,`${name}`)).then(()=>{
-            resolve('Directory Created')
+            resolve('Directory Created');
         }).catch(err=> reject(err));
     });
 }
@@ -18,27 +18,50 @@ const createReactApp = (name)=>{
         spawn('npx',['create-react-app','client'],{
             cwd: path.join(_cwd,name),
             stdio:"inherit"
-        })
+        });
         resolve('success');
     })
 }
+
+const installDep =(name)=>{
+    return new Promise((resolve,reject)=>{
+        spawn('npm',['install'],{
+            cwd:path.join(_cwd,name),
+            stdio:'inherit'
+        });
+        resolve('success');
+    })
+}
+
 const run = async () => {
     const questions = await inquirer.prompt([
         {
             type: 'input',
             name: 'projectName',
             message: 'What is the name of the project? (Use small letters and without space)'
+        },
+        {
+            type: 'confirm',
+            name:"installDep",
+            message:"Install Node Modules for Express App?(y)",
+            default: "yes"
         }
     ]);
-    // console.log(questions);
-    // var a=spawn('ls',['-al'],{
-    //     cwd:path.join(_cwd,'template'),
-    //     stdio:"inherit"
-    // });
-    // console.log(a);
-    await createDir(questions.projectName);
-    // await createReactApp(questions.projectName);
-    fs.copySync(path.join(__dirname,'template/'),path.join(_cwd,questions.projectName));
+    console.log(questions);
+    await createDir(questions.projectName).then((data)=>{
+        console.log('\x1b[32m%s\x1b[0m',"Directory Created");
+    })
+
+    await createReactApp(questions.projectName).then((data)=>{
+        console.log('\x1b[32m%s\x1b[0m',"ReactJS Added");
+    })
+
+    fs.copy(path.join(__dirname,'template/'),path.join(_cwd,questions.projectName),(err)=>{
+        if(!err)
+            console.log('\x1b[32m%s\x1b[0m',"Express App Generated");
+    })
+
+    questions.installDep ? installDep(questions.projectName) : console.log('\x1b[41m%s\x1b[0m',"run npm install after finished setting up");
 
 }
 
