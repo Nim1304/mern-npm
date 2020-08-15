3bee00a0d4d502bcc775ac9e3a1fb8cd65187b38
Nimish Shah <nimishshah2000@gmail.com>
2020-08-15 12:11:03 +0530

    cli args

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const { spawn } = require('child_process');
+const spawn = require('child_process').spawn;
 const inquirer = require('inquirer');
 const path = require('path');
-const _cwd = process.cwd();
 const fs = require('fs-extra');
+const commander = require('commander');
+const _cwd = process.cwd();
 
 const createDir = (name) => {
     return new Promise((resolve, reject) => {
@@ -22,10 +23,10 @@ const createReactApp = (name) => {
             cwd: path.join(_cwd, name),
             stdio: "inherit"
         });
-        command.on('close',_ => {
+        command.on('close', _ => {
             resolve();
         })
-        command.on('error',err => {
+        command.on('error', err => {
             reject(err);
         })
     })
@@ -37,10 +38,10 @@ const installDep = (name) => {
             cwd: path.join(_cwd, name),
             stdio: 'inherit'
         });
-        command.on('close',_ => {
+        command.on('close', _ => {
             resolve();
         })
-        command.on('error',err => {
+        command.on('error', err => {
             reject(err);
         })
     })
@@ -53,50 +54,67 @@ const gitInit = (name) => {
     });
 }
 
+const showHelp = () => {
+    console.log("There's been some changes since last update... :)\n");
+    console.log("usage: init-mern [<projectname>] [options]\n\n");
+    console.log("Options:\n");
+    console.log(` -g, --git         Initialize with git repo\n -i, --install     Install Express App dependencies\n -h, --help        Show this help menu`);
+}
 const run = async () => {
-    const questions = await inquirer.prompt([
-        {
-            type: 'input',
-            name: 'projectName',
-            message: 'What is the name of the project? (Use small letters and without space, leave blank for using current directory)'
-        },
-        {
-            type: 'confirm',
-            name: "installDep",
-            message: "Install Node Modules for Express App?(y)",
-            default: "yes"
-        },
-        {
-            type: 'confirm',
-            name: 'gitInit',
-            message: "Initialize with git ?",
-            default: 'no'
-        }
-    ]);
-    // console.log(questions);
-    questions.projectName = questions.projectName.trim()
-    console.log(questions)
-    await createDir(questions.projectName).then((data) => {
-        console.log('\x1b[32m%s\x1b[0m', data);
-    })
+    if (process.argv.length > 2) {
+        const questions = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'projectName',
+                message: 'What is the name of the project? (Use small letters and without space, leave blank for using current directory)'
+            },
+            {
+                type: 'confirm',
+                name: "installDep",
+                message: "Install Node Modules for Express App?(y)",
+                default: "yes"
+            },
+            {
+                type: 'confirm',
+                name: 'gitInit',
+                message: "Initialize with git ?",
+                default: 'no'
+            }
+        ]);
+        // console.log(questions);
+        questions.projectName = questions.projectName.trim()
+        console.log(questions)
+        await createDir(questions.projectName).then((data) => {
+            console.log('\x1b[32m%s\x1b[0m', data);
+        })
 
-    await createReactApp(questions.projectName).then((data) => {
-        console.log('\x1b[32m%s\x1b[0m', "ReactJS Added");
-    }).catch(err => {
-        console.log('x1b[41m%s\x1b[0m',err);
-    })
+        await createReactApp(questions.projectName).then((data) => {
+            console.log('\x1b[32m%s\x1b[0m', "ReactJS Added");
+        }).catch(err => {
+            console.log('x1b[41m%s\x1b[0m', err);
+        })
 
-    fs.copy(path.join(__dirname, 'template/'), path.join(_cwd, questions.projectName), async (err) => {
-        if (!err) {
-            var packageData = JSON.parse(fs.readFileSync(path.join(_cwd, questions.projectName, 'package.json')))
-            packageData['name'] = questions.projectName;
-            fs.writeFileSync(path.join(_cwd, questions.projectName, 'package.json'), JSON.stringify(packageData, null, 2));
-            console.log('\x1b[32m%s\x1b[0m', "Express App Generated");
-        }
-    })
+        fs.copy(path.join(__dirname, 'template/'), path.join(_cwd, questions.projectName), async (err) => {
+            if (!err) {
+                var packageData = JSON.parse(fs.readFileSync(path.join(_cwd, questions.projectName, 'package.json')))
+                packageData['name'] = questions.projectName;
+                fs.writeFileSync(path.join(_cwd, questions.projectName, 'package.json'), JSON.stringify(packageData, null, 2));
+                console.log('\x1b[32m%s\x1b[0m', "Express App Generated");
+            }
+        })
 
-    questions.installDep ? installDep(questions.projectName) : console.log('\x1b[41m%s\x1b[0m', "run npm install after finished setting up");
-    questions.gitInit ? gitInit(questions.projectName) : console.log('\x1b[41m%s\x1b[0m', "Git not initialized");
+        questions.installDep ? installDep(questions.projectName) : console.log('\x1b[41m%s\x1b[0m', "run npm install after finished setting up");
+        questions.gitInit ? gitInit(questions.projectName) : console.log('\x1b[41m%s\x1b[0m', "Git not initialized");
+    } else {
+        console.log('lol');
+    }
 }
 
-run();
+// run();
+// showHelp()
+
+const args = commander.program
+                        .option('-g, --git','Initialize with empty git repo',false)
+                        .option('-i, --install','Install Express App dependencied',false).parse(process.argv);
+
+console.log(args);
